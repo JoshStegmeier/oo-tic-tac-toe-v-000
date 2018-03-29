dcc500c9ff2e124606e6131491b70e6fa4205ee7
Josh Stegmeier <josh@steg.me>
2018-03-29 22:59:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,11 +15,11 @@ class TicTacToe
   ]
 
   def display_board
-    print " #{board[0]} " "|" " #{board[1]} " "|" " #{board[2]} \n"
+    print " #{@board[0]} " "|" " #{@board[1]} " "|" " #{@board[2]} \n"
     print "-----------\n"
-    print " #{board[3]} " "|" " #{board[4]} " "|" " #{board[5]} \n"
+    print " #{@board[3]} " "|" " #{@board[4]} " "|" " #{@board[5]} \n"
     print "-----------\n"
-    print " #{board[6]} " "|" " #{board[7]} " "|" " #{board[8]} \n"
+    print " #{@board[6]} " "|" " #{@board[7]} " "|" " #{@board[8]} \n"
   end
 
   # input_to_index
