2740ede7dfc6ba16051a5b0c3f0dd6a780056254
Josh Stegmeier <josh@steg.me>
2018-03-29 23:06:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,7 +14,7 @@ class TicTacToe
     [2, 4, 6]  #diagonal2
   ]
 
-  def display_sboard
+  def display_board
     print " #{@board[0]} " "|" " #{@board[1]} " "|" " #{@board[2]} \n"
     print "-----------\n"
     print " #{@board[3]} " "|" " #{@board[4]} " "|" " #{@board[5]} \n"
